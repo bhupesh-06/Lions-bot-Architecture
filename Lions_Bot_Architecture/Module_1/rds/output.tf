f9e628168e340c1aba4ca3947b30c8a9b3877d2c
Bhupesh Dubey <bhupeshdubey06@outlook.com>
2023-12-04 18:50:37 +0530

    Delete Lions_Bot_Architecture directory

--- Lions_Bot_Architecture/Module_1/rds/output.tf
+++
@@ -1,8 +0,0 @@
-
-# output "db_instance_id" {
-#   value = aws_db_instance.default.identifier
-# }
-
-# output "db_instance_address" {
-#   value = aws_db_instance.default.address
-# }
--- Lions_Bot_Architecture/Module_2/waf/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "web_acl_arn" {
-  description = "The ARN of the WAF WebACL."
-  value       = aws_wafv2_web_acl.WafWebAcl.arn
-}
-
